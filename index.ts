7e954e07352f1c030bedbeb32d8ee313b3533381
tqsmmm <tqsmmm@gmail.com>
2023-03-03 16:29:51 +0800

    更新后端接口

--- index.ts
+++ index.ts
@@ -1,16 +1,43 @@
 import express, { Express, Router, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 
-app.use('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-});
+app.use(
+    cors({
+        origin: ['http://localhost:5173'],
+        // credentials: true,
+    })
+);
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(bodyParser.json());
 
 const router: Router = express.Router();
 
 app.use('/api', router);
 
-router.get('/login', async (req: Request, res: Response) => {});
+router.post('/login', async (req: Request, res: Response) => {
+    console.log(req.body);
+
+    if (req.body.user_id == 'admin' && req.body.user_pwd == 'admin') {
+        res.json({
+            code: 200,
+            message: 'Success',
+        });
+    } else {
+        res.json({
+            code: 400,
+            message: 'Error',
+        });
+    }
+});
 
 app.listen(5000, () => {
     console.log('App running at: http://localhost:5000/');
